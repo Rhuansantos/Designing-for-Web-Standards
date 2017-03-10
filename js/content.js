a91fb3f286ae4b9618ee5f3f52cc4764b87a5f95
Rhuan Souza <rhuansantosdev@gmail.com>
2017-03-09 21:09:21 -0500

    Loading posts from blog

--- js/content.js
+++ js/content.js
@@ -5,10 +5,41 @@
   
       var objData = JSON.parse(objRequest.responseText);
       console.log(objData);
+
+
+
+    loadPosts(objData.posts);
+
+     
     
   };
 
   objRequest.send();
 
+
+
+function loadPosts(_data){
+    // setting local variables
+    var data = _data;
+    var container = document.querySelector('#post');
+    var posts = '';
+
+    // Checking if the element is on the page
+    if(container !== null){
+
+        for (i = 0; i < data.length; i++) {
+         posts += '<article>';
+         posts += '<img src="' + data[i].imageURL + '" alt="">';
+         posts += '<h1>' + data[i].title + '</h1>';
+         posts += '<p>' + data[i].text + '</p>';
+         posts += '</article>';
+        }
+
+        // print the html
+         container.innerHTML = posts;
+       
+    }
+
+ }
 
 
