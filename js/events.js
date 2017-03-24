acdba74c13ec0346b1504bc34d75a70b17e8cea9
Rhuan Souza <rhuansantosdev@gmail.com>
2017-03-23 22:33:25 -0400

    testing window.location.pathname on githubPages

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
+// current URL
+var currentUrl = window.location.pathname;
+
+
+// Form variables
 var userName = document.querySelector('#name');
 var email 	= document.querySelector('#email');
 var website	= document.querySelector('#website');
 var comment = document.querySelector('#comment');
+
+// controler
+
+if(currentUrl == "/blog.html" || currentUrl == "/blog.html/"){
+
+	alert("oi");
+}
+
+if(userName !== null && email !== null && website !== null && comment !== null) {
+
+	userName.addEventListener('blur',formValidation);
+	email.addEventListener('blur',formValidation);
+	website.addEventListener('blur',formValidation);
+	comment.addEventListener('blur',formValidation);
+
+}
+
+
+
 
 function formValidation(event){
 
@@ -20,7 +44,13 @@ function formValidation(event){
 
 }
 
-userName.addEventListener('blur',formValidation);
-email.addEventListener('blur',formValidation);
-website.addEventListener('blur',formValidation);
-comment.addEventListener('blur',formValidation);
+
+
+
+
+
+
+function blogPagination(){
+
+
+}
