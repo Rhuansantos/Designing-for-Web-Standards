edcfa4c4e1ac330bb7562674a71538479b9f727b
Rhuan Souza <rhuansantosdev@gmail.com>
2017-03-24 13:33:49 -0400

    Required fields for e-mail

--- js/events.js
+++ js/events.js
@@ -1,58 +1,51 @@
 // current URL
 var currentUrl = window.location.pathname;
 
-console.log(currentUrl);
-
 
 // Form variables
 var userName = document.querySelector('#name');
 var email 	= document.querySelector('#email');
 var website	= document.querySelector('#website');
 var comment = document.querySelector('#comment');
+var submit = document.querySelector('input[type="submit"]');
 
 // controler
 
-if(currentUrl == "/Designing-for-Web-Standards/blog.html"){
+if(currentUrl == "/Designing-for-Web-Standards/blog.html" || currentUrl == "/FullSail/Designing-for-Web-Standards/blog.html"){
 
-	alert("oi");
+	blogPagination();
 }
 
 if(userName !== null && email !== null && website !== null && comment !== null) {
 
 	userName.addEventListener('blur',formValidation);
 	email.addEventListener('blur',formValidation);
-	website.addEventListener('blur',formValidation);
-	comment.addEventListener('blur',formValidation);
 
 }
-
 
 
 
 function formValidation(event){
 
 		var field = event.target;
+	 	var alert = field.parentElement.querySelector('.alert');
     	var errorMsg = '<p class="alert">This field can not be empty</p>';
-
+   
 
         if (field.value === "") {
       	
             field.parentElement.insertAdjacentHTML('beforeend', errorMsg);
+        }	
+         if (alert !== null) {
+                alert.remove();
+            }         
               
-        }
-
-      
-
 
 }
 
 
+// function blogPagination(){
+	
+// 	var blogArticles = document.querySelector('#post article');
 
-
-
-
-
-function blogPagination(){
-
-
-}
+// }
